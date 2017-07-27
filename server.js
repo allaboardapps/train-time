86cbd8cd7df01d7c7cd33173bb1d92bec3401ab0
Randy Burgess <wrburgess@gmail.com>
2017-07-27 18:59:52 -0500

    testing out morgan and full request

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const CONCURRENCY = process.env.WEB_CONCURRENCY || 1
 const PORT = process.env.PORT || 8080
 
 const express = require("express")
+const morgan = require('morgan')
 const bodyParser = require("body-parser")
 
 const app = express()
+app.use(morgan('combined'))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
@@ -13,9 +15,10 @@ app.get('/', (req, res) => {
 
 app.post('/trains', (req, res) => {
   if (!req.body) return res.json({ 'error': 'No request body present' })
-  res.json({ 'data': `${req.body.test}` })
+  console.log(JSON.stringify(req.body, null, 2))
+  res.json(req.body)
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, (req, res) => {
   console.log(`Server is running on port ${PORT}`)
 })
